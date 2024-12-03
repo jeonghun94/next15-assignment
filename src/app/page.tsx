33094fbd3f1b845e16b0d8576c50d2ea76062d0a
jeonghun94 <jeongh1021@naver.com>
2024-12-03 10:02:58 +0900

    overwirte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,5 @@
 "use client";
 
-import { useActionState, useState } from "react";
-import handleLoginForm from "./actions";
-import FormInput from "./components/FormInput";
-import FormBtn from "./components/FormButton";
-import Message from "./components/Message";
-
 export default function Home() {
-    const [state, action] = useActionState(handleLoginForm, null);
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.target as HTMLFormElement);
-        action(formData);
-    };
-
-    return (
-        <div className="w-full h-screen flex flex-col items-center justify-center gap-4 ">
-            <form className="flex flex-col gap-4 w-1/5" onSubmit={handleSubmit}>
-                <FormInput placeholder="이메일" type="email" name="email" errors={state?.fieldErrors?.email} />
-                <FormInput
-                    placeholder="사용자 이름"
-                    type="text"
-                    name="username"
-                    errors={state?.fieldErrors?.username}
-                />
-                <FormInput
-                    placeholder="비밀번호"
-                    type="password"
-                    name="password"
-                    errors={state?.fieldErrors?.password}
-                />
-                <FormBtn />
-                {state?.success && <Message type="success" message="Welcome Back" />}
-            </form>
-        </div>
-    );
+  return <main className="flex flex-col gap-10 items-center justify-center">HOME</main>;
 }
---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { getSession } from "@/utils/session";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Routes {
+    [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+    "/log-in": true,
+    "/create-account": true,
+};
+
+export async function middleware(request: NextRequest) {
+    const session = await getSession();
+
+    console.log(session);
+
+    const isPublicUrl = publicOnlyUrls[request.nextUrl.pathname];
+    if (!session.id) {
+        if (!isPublicUrl) {
+            return NextResponse.redirect(new URL("/log-in", request.url));
+        }
+    } else {
+        if (isPublicUrl) {
+            return NextResponse.redirect(new URL("/profile", request.url));
+        }
+    }
+}
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
